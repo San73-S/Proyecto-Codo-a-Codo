0edd6ae4c0406d24e6c29851e7874b35efe2a111
Santiago Santos <seba00001s@gmail.com>
2022-05-18 12:40:03 -0300

    Iniciando proyecto

---
+++ js/movil.js
@@ -0,0 +1,7 @@
+function deslizarDr(){
+    document.getElementById("barra-lat").classList='barra-lateral animacionBarraDr';
+}
+
+function deslizarIz(){
+    document.getElementById("barra-lat").classList='barra-lateral animacionBarraIz';
+}
