8db98e0bf75df5b65ca0883e7ee52ad5efae55f4
Santiago Santos <seba00001s@gmail.com>
2022-05-19 14:51:58 -0300

    Animacion de los tren puntos realizada en js de manera correcta

--- js/index.js
+++ js/index.js
@@ -3,58 +3,124 @@ let s2 = document.getElementById("s2");
 let s3 = document.getElementById("s3");
 
 (function () {
-    let contador = 1;
-    let tiempo = 4000;
-    let flag = false;
+    // let contador = 1;
+    // let tiempo = 4000;
+    // let flag = false;
 
-    let pintar = function () {
-        console.log(contador)
+    // let pintar = function () {
+    //     console.log(contador)
+
+    //     if (contador === 1) {
+    //         s1.style.backgroundColor = 'var(--color-vioCla)';
+    //         s2.style.backgroundColor = 'white';
+    //         s3.style.backgroundColor = 'white';
+    //         flag = false;
+    //     }
+
+    //     if (contador === 2) {
+    //         s1.style.backgroundColor = 'white';
+    //         s2.style.backgroundColor = 'var(--color-vioCla)';
+    //         s3.style.backgroundColor = 'white';
+    //         console.log(tiempo)
+    //     }
+
+    //     if (flag == true) {
+    //         contador -= 2;
+    //     }
+
+    //     if (contador === 3) {
+    //         s2.style.backgroundColor = 'white';
+    //         s3.style.backgroundColor = 'var(--color-vioCla)';
+    //         contador -= 2;
+    //         flag = true;
+    //         console.log(tiempo)
+    //     }
+
+    //     contador++;
+    // };
+
+    // s1.style.backgroundColor = 'var(--color-vioCla)';
+    // contador++;
+    // setInterval(pintar, tiempo);
+
+    /*ESTA FUNCION LA UTILIZO PARA LA ANIMACION DE LOS TRES PUNTOS EN EL SLIDER*/
+
+
+
+             /*///----------EXPLICACION DE LO QUE HACE PARA MI (:---------\\\*/
+
+    /*Al no hacer el slider con js tuve que crear la animacion de los tres puntos con js
+    Ya que esta tiende a retroceder en sus pasos al terminar y a su vez modifica su tiempo.
+    el slider tiene un patron de animacion que es (Secuencia de fotos: 1 - 2 - 3 - |2| - 1 - 2 - 3 - |2|...)
+    el slider esta creado para durar 10 seg, pero al volver en sus pasos la ultima foto en vez
+    de durar 4 seg dura 6, lo mismo le pasa a la primer foto en su regreso.*/
+    
+    /*La animacion comienza con un tiempo de 4s, pero cuando esta llega a su ultima foto tarda 6 seg
+    en volver a estar parado en la segunda foto. a travez de un contador que solo puede tener como
+    valor 1, 2 y 3 ya que modifico su valor con condiciones cuando necesito que sea necesario
+    voy cambiandole el color a los diferentes puntos.
+    Cuando el contador esta en 3, este disminuye en 2 y su delay incrementa en 6s, el contador 
+    por cada vuelta se incrementa en uno, lo que nos da un resultado de 3 - 2 + 1 = 2.
+    Es decir que vamos a pintar el circulo 2. en el if que se encarga de verificar si el contador es 2
+    tambien se encarga de verificar si el delay esta en 6s, si este esta en 6s lo cambia a 4s
+    y con la ayuda de un Flag auxiliar que comienza siendo false entra a otra condicion que se encarga
+    de cambiar el valor al Flag principal, lo pasa de falso a verdadero. este Flag principal
+    tiene como objetivo restarle 2 al contador, incrementar el delay en 6s y pasar a true al Flag auxiliar,
+    este mismo pasa a true ya que si permanece en false al entrar en el contador == 2 este volver a poner el
+    Flag principal en true y estarimos dando vueltas en el contador 1 y 2. este segundo flag tiene como 
+    finalidad evitar que esto pase y que el contador pueda seguir una secuencia correcta. */ 
+
+    let delay = 4000;
+    let contador = 1;
+    let flag = false;
+    let flagDos = false;
+
+    s1.style.backgroundColor = 'var(--color-vioCla)';
+    contador++
+
+    let timerId = setTimeout(function pintar() {
+        
+        console.log(delay)
+
+        if (flag == true && delay == 4000) {
+            contador -= 2;
+            delay = 6000;
+            flagDos = true;
+        }
 
         if (contador === 1) {
             s1.style.backgroundColor = 'var(--color-vioCla)';
             s2.style.backgroundColor = 'white';
             s3.style.backgroundColor = 'white';
-            flag = false;
+            flag = false;      
         }
 
         if (contador === 2) {
             s1.style.backgroundColor = 'white';
             s2.style.backgroundColor = 'var(--color-vioCla)';
             s3.style.backgroundColor = 'white';
-            console.log(tiempo)
-        }
 
-        if(flag == true){
-            contador -=2;
-        }
+            if(delay == 6000){          
+               delay = 4000;
+               if(flagDos != true){
+                  flag = true;                   
+               } else{
+                  flagDos = false;   
+               }               
+            }            
+        }        
 
         if (contador === 3) {
             s2.style.backgroundColor = 'white';
             s3.style.backgroundColor = 'var(--color-vioCla)';
-            contador -= 2;
-            flag = true;
-            console.log(tiempo)
+            contador -= 2;            
+            delay = 6000;
         }
 
         contador++;
-    };
 
-    s1.style.backgroundColor = 'var(--color-vioCla)';
-    contador++;
-    setInterval(pintar, tiempo);
+        timerId = setTimeout(pintar, delay);
 
-//     let delay = 5000;
-
-//     let timerId = setTimeout(function request() {
-//   ...enviar solicitud...
-
-//   if (solicitud fallida debido a sobrecarga del servidor) {
-//         //aumentar el intervalo en la próxima ejecución
-//         delay *= 2;
-//     }
-
-//     timerId = setTimeout(request, delay);
-
-// }, delay);
+    }, delay);
 
 }());
